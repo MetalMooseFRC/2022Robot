12259d2ce702f4d9217117f509c6616851b4ff25
Isaac R <isaac.t.rudnick@gmail.com>
2022-01-12 16:23:12 -0500

    Created ColorSensor base

--- src/main/java/frc/robot/subsystems/ColorSensor.java
+++ src/main/java/frc/robot/subsystems/ColorSensor.java
@@ -4,16 +4,14 @@
 
 package frc.robot.subsystems;
 
-import edu.wpi.first.wpilibj.command.Subsystem;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
-/** Add your docs here. */
-public class ColorSensor extends Subsystem {
-  // Put methods for controlling this subsystem
-  // here. Call these from Commands.
+public class ColorSensor extends SubsystemBase {
+  /** Creates a new ColorSensor. */
+  public ColorSensor() {}
 
   @Override
-  public void initDefaultCommand() {
-    // Set the default command for a subsystem here.
-    // setDefaultCommand(new MySpecialCommand());
+  public void periodic() {
+    // This method will be called once per scheduler run
   }
 }
