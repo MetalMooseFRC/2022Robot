6a533f646194e41b4bdfaf4b777e8645acd5ab69
Isaac R <isaac.t.rudnick@gmail.com>
2022-01-13 17:16:34 -0500

    Color Sensing Code (Not accurate for red)

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -4,6 +4,15 @@
 
 package frc.robot;
 
+import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj.I2C;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj.util.Color;
+
+import com.revrobotics.ColorSensorV3;
+import com.revrobotics.ColorMatchResult;
+import com.revrobotics.ColorMatch;
+
 /**
  * The Constants class provides a convenient place for teams to hold robot-wide numerical or boolean
  * constants. This class should not be used for any other purpose. All constants should be declared
@@ -12,4 +21,25 @@ package frc.robot;
  * <p>It is advised to statically import this class (or one of its inner classes) wherever the
  * constants are needed, to reduce verbosity.
  */
-public final class Constants {}
+public final class Constants {
+
+	public static final int MOTOR_LEFT_1_ID = 4;
+	public static final int MOTOR_LEFT_2_ID = 5;
+	public static final int MOTOR_LEFT_3_ID = 6;
+    
+    public static final int MOTOR_RIGHT_1_ID = 1;
+    public static final int MOTOR_RIGHT_2_ID = 2;
+	public static final int MOTOR_RIGHT_3_ID = 3;
+	
+	public static final int DRIVER_STICK_PORT = 0;
+	
+	public static final double MIN_JOYSTICK_INPUT = 0.1;
+	public static final double JOYSTICK_SPEED_FACTOR = 1.2;
+	public static final double JOYSTICK_TURN_FACTOR = 1.2;
+	
+	
+	public static final double TARGET_HEIGHT = 57;  //inches
+	public static final double LIMELIGHT_HEIGHT = 39.25; //inches
+
+	public static final double COLOR_DIFFERENCE = 0.15;
+}
--- src/main/java/frc/robot/subsystems/ColorSensor.java
+++ src/main/java/frc/robot/subsystems/ColorSensor.java
@@ -6,12 +6,50 @@ package frc.robot.subsystems;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
+import edu.wpi.first.wpilibj.I2C;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj.util.Color;
+import frc.robot.Constants;
+
+import com.revrobotics.ColorSensorV3;
+import com.revrobotics.ColorMatchResult;
+import com.revrobotics.ColorMatch;
+
 public class ColorSensor extends SubsystemBase {
+  
+
+  private final I2C.Port i2cPort = I2C.Port.kOnboard;
+  private final ColorMatch m_colorMatcher = new ColorMatch();
+  private final ColorSensorV3 m_colorSensor = new ColorSensorV3(i2cPort);
+  
+
   /** Creates a new ColorSensor. */
   public ColorSensor() {}
+
+
 
   @Override
   public void periodic() {
     // This method will be called once per scheduler run
+    Color detectedColor = m_colorSensor.getColor();
+
+    final double red = detectedColor.red;
+    final double blue = detectedColor.blue;
+
+    if (red - blue > Constants.COLOR_DIFFERENCE){
+      SmartDashboard.putString("color", "Red");
+    }
+    if (blue - red > Constants.COLOR_DIFFERENCE){
+      SmartDashboard.putString("color", "Blue");
+    }
+    else {
+      SmartDashboard.putString("color", "Uknown?");
+    }
+
+
+    SmartDashboard.putNumber("Red", detectedColor.red);
+    SmartDashboard.putNumber("Green", detectedColor.green);
+    SmartDashboard.putNumber("Blue", detectedColor.blue);
+
   }
 }
