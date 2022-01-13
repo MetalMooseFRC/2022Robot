3ffca3fe782797dec6a1ab4eba0754a4fe95bfb4
Isaac R <isaac.t.rudnick@gmail.com>
2022-01-13 17:20:44 -0500

    Added ColorSensor Comments

--- src/main/java/frc/robot/subsystems/ColorSensor.java
+++ src/main/java/frc/robot/subsystems/ColorSensor.java
@@ -33,20 +33,24 @@ public class ColorSensor extends SubsystemBase {
     // This method will be called once per scheduler run
     Color detectedColor = m_colorSensor.getColor();
 
+    // red value detected
     final double red = detectedColor.red;
+    // blue value detected
     final double blue = detectedColor.blue;
 
+    // If color is greater than other by COLOR_DIFFERENCE, then put on screen
     if (red - blue > Constants.COLOR_DIFFERENCE){
       SmartDashboard.putString("color", "Red");
     }
     if (blue - red > Constants.COLOR_DIFFERENCE){
       SmartDashboard.putString("color", "Blue");
     }
+    // otherwise put unknown
     else {
       SmartDashboard.putString("color", "Uknown?");
     }
 
-
+    // Put all colors for debugging
     SmartDashboard.putNumber("Red", detectedColor.red);
     SmartDashboard.putNumber("Green", detectedColor.green);
     SmartDashboard.putNumber("Blue", detectedColor.blue);
