6c58853b2e87658cd5e5af95fd97fe60f991cd52
Isaac R <isaac.t.rudnick@gmail.com>
2022-02-01 17:36:34 -0500

    Working untuned Turret Code

--- src/main/java/frc/robot/commands/TurretControl.java
+++ src/main/java/frc/robot/commands/TurretControl.java
@@ -18,7 +18,6 @@ public class TurretControl extends CommandBase {
   private DoubleSupplier m_angle;
 
   // TODO: Need to make this constants and tune properly
-  private final PIDController m_leftpidController = new PIDController(0.2, 0, 0);
   private final PIDController m_rightpidController = new PIDController(0.2, 0, 0);
 
 
@@ -33,11 +32,16 @@ public class TurretControl extends CommandBase {
   // Called when the command is initially scheduled.
   @Override
   public void initialize() {
+    System.out.println("Initialized!!!");
     // Reset position
     m_turret.leftTurretEncoder.setPosition(0);
+    m_turret.rightTurretEncoder.setPosition(0);
+
+    // Set set points
+    // THE PID loop is trying to get 
+    m_rightpidController.setSetpoint(0);
 
     // TODO: Tune / make constant
-    m_leftpidController.setTolerance(0.2);
     m_rightpidController.setTolerance(0.2);
 
   }
@@ -45,38 +49,41 @@ public class TurretControl extends CommandBase {
   // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
-    m_leftpidController.setSetpoint(0);
-    m_rightpidController.setSetpoint(0);
+    double leftMove = Math.max(-.1, Math.min(.1, m_angle.getAsDouble()));
+    System.out.println("EXECUTING!!!!");
+    m_turret.motorLeft.set(leftMove);
+    // Difference between double the left turret motor and right turret (we want this number to be zero)
+    double PIDinput = m_turret.rightTurretEncoder.getPosition() - 2*m_turret.leftTurretEncoder.getPosition();
+    double rightpidOutput = m_rightpidController.calculate(PIDinput);
 
-    double LeftpidOutput = m_leftpidController.calculate(m_turret.leftTurretEncoder.getPosition());
-    double RightpidOutput = m_rightpidController.calculate(m_turret.rightTurretEncoder.getPosition());
-
-    SmartDashboard.putNumber("LeftpidOutput Turret", LeftpidOutput);
-    SmartDashboard.putNumber("RightpidOutput Turret", RightpidOutput);
+    //SmartDashboard.putNumber("LeftpidOutput Turret", LeftpidOutput);
+    SmartDashboard.putNumber("rightpidOutput Turret", rightpidOutput);
 
     
     //if (pidOutput < 0){pidOutput -= Constants.VISION_FF;}
     //else if (pidOutput > 0){pidOutput += Constants.VISION_FF;}
     // Make sure PID Controller is capped to 1 (more would be bad)
     double clamp = .1;
-    LeftpidOutput = Math.max(-clamp, Math.min(clamp, LeftpidOutput));
-    RightpidOutput = Math.max(-clamp, Math.min(clamp, RightpidOutput));
+    //LeftpidOutput = Math.max(-clamp, Math.min(clamp, LeftpidOutput));
+    rightpidOutput = Math.max(-clamp, Math.min(clamp, rightpidOutput));
 
-    m_turret.rightTurretEncoder.set(LeftpidOutput);
-    m_turret.leftTurretEncoder.set(RightpidOutput);
+    System.out.println(rightpidOutput);
+    m_turret.motorRight.set(rightpidOutput);
   }
 
   // Called once the command ends or is interrupted.
   @Override
   public void end(boolean interrupted) {
-    m_turret.leftTurretEncoder.set(0);
-    m_turret.rightTurretEncoder.set(0);
+    m_turret.leftTurretEncoder.setPosition(0);
+    m_turret.rightTurretEncoder.setPosition(0);
   }
 
   // Returns true when the command should end.
   @Override
   public boolean isFinished() {
-    if(m_pidController.atSetpoint()){return true;}
+    // Can't end if a default command
+    // if(m_leftpidController.atSetpoint() && m_rightpidController.atSetpoint()){return true;}
+    // else {return false;}
     return false;
   }
 }
